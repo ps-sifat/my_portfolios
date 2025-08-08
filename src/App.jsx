c803d1a0c9e22b6b1e7c4f2cf1fa700dc898e525
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2025-08-08 17:43:33 +0600

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './assets/Component/Navbar';
+import React from "react";
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Navbar/>} />
-    </Routes>
-    </BrowserRouter>
-  )
-}
+    <div className="font-primary h-screen bg-hero-pattern">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur in quo
+      sint excepturi similique quia omnis quam? Consectetur temporibus magnam
+      maxime quisquam libero sunt sapiente, fugit culpa! Quae, cumque dolores.
+    </div>
+  );
+};
 
-export default App
+export default App;
