3b8dcb6b829fd904963493909f971f21a497dc28
ps-sifat <mdsifatulislam2357@gmail.com>
2026-07-08 06:42:07 +0600

    after few days update my portfolios

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,13 +3,12 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 import "react-vertical-timeline-component/style.min.css";
 
-import { styles } from "../styles";
+import { styles } from "../style";
 import { experiences } from "../constants";
-import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
 
 const ExperienceCard = ({ experience }) => {
